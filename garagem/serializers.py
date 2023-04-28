6334ec99f666f22e8d05f997cc02ea3fdd4b92e4
VictoorLDS <vlds221005@gmail.com>
2023-04-28 16:46:30 -0300

    API Veiculos, Acessorios feitas

--- garagem/serializers.py
+++ garagem/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from garagem.models import Categoria, Marca
+from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo
 
 class MarcaSerializer(ModelSerializer):
     class Meta:
@@ -11,3 +11,30 @@ class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = "__all__"
+
+class CorSerializer(ModelSerializer):
+    class Meta:
+        model = Cor
+        fields = "__all__"
+
+class AcessorioSerializer(ModelSerializer):
+    class Meta:
+        model = Acessorio
+        fields = "__all__"
+
+
+class VeiculoSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "_all__"
+
+class VeiculoDetSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
+        depth = 1
+
+class VeiculoListSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = ["Modelo", "Ano", "Preço"]
--- garagem/views.py
+++ garagem/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Categoria, Marca
-from garagem.serializers import CategoriaSerializer, MarcaSerializer
+from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo
+from garagem.serializers import AcessorioSerializer, CategoriaSerializer,CorSerializer, MarcaSerializer, VeiculoSerializer, VeiculoDetSerializer, VeiculoListSerializer
 
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
@@ -10,3 +10,17 @@ class MarcaViewSet(ModelViewSet):
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = {"list": VeiculoListSerializer, "retrieve": VeiculoDetSerializer}
+    def get_serializer_class(self):
+        return self.serializer_class.get(self.action, VeiculoSerializer)
